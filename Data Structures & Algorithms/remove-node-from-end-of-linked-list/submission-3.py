2827acfe45dfcfae36888a4eda41f22db81b46ec
Eric Zhao <146782392+eericzzhao@users.noreply.github.com>
2026-08-24 18:35:22 -0700

    Add: remove-node-from-end-of-linked-list - submission-3

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-3.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        initial = ListNode(6, head)
+        left = initial
+        right = head 
+
+        for i in range(n):
+            right = right.next 
+        
+        while right:
+            left = left.next
+            right = right.next
+        
+        # delete 
+        left.next = left.next.next 
+        return initial.next
+        
+        
+        
